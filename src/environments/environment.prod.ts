4e90c954db9eef98242500ff7db6d4a4c53f890a
iLalLoTiX <lalotix@hotmail.com>
2020-12-21 10:38:33 -0600

    pruebas con pm2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  base_url:'http://192.168.1.103/api/api'
+  base_url:'http://192.168.1.120:3000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: true,
-  base_url:'http://localhost:3000/api'
+  base_url:'http://192.168.1.120:3000/api'
 };
